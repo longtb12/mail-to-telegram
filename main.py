fd20a8faadf9d7098251538a7b28bf8d039c8171
longtb12 <longtb11@gmail.com>
2025-04-26 18:45:14 +0700

    fix issue and add extrat from netflix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import re
 
 # Load variables from .env into environment
 load_dotenv()
@@ -16,7 +17,7 @@ tele_token = os.getenv("TELEGRAM_TOKEN")
 chat_id = os.getenv("CHAT_ID")
 sleep_time = int(os.getenv("SLEEPTIME"))
 allowed_sender = os.getenv("ALLOWED_SENDER")
-tele_url = 'https://api.telegram.org/bot{tele_token}/sendMessage'
+tele_url = f'https://api.telegram.org/bot{tele_token}/sendMessage'
 
 # Set up logging
 logging.basicConfig(filename='gmail_to_telegram.log', level=logging.INFO)
@@ -45,7 +46,7 @@ def get_email_details(mail, email_id):
         if sender in allowed_sender and msg.is_multipart():
             for part in msg.walk():
                 if part.get_content_type() == 'text/plain':
-                    body = part.get_payload(decode=True).decode()[:500]
+                    body = part.get_payload(decode=True).decode()
                     break
         
         return {'subject': subject, 'from': from_, 'body': body}
@@ -55,7 +56,8 @@ def get_email_details(mail, email_id):
 
 def send_to_telegram(email_data):
     try:
-        text = f"New Email\nFrom: {email_data['from']}\nSubject: {email_data['subject']}\nBody: {email_data['body']}"
+        match = re.search(r"https:\/\/www\.netflix\.com\/account\/travel\/verify[^\s\]]+", email_data['body'])
+        text = f"New Email\nFrom: {email_data['from']}\nBody: {match.group()}"
         response = requests.post(tele_url, data={'chat_id': chat_id, 'text': text})
         logging.info(f"{datetime.now()}: {response}")
         if response.ok:
